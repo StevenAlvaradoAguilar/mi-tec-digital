91b948823a7b986180542ed0704b92a49fc7d15f
StevenAlvaradoAguilar <alvaradosteven6@gmail.com>
2021-03-05 19:26:10 -0600

    Código terminado agrego comentarios

--- src/main/java/tec/bd/app/service/EstudianteService.java
+++ src/main/java/tec/bd/app/service/EstudianteService.java
@@ -26,39 +26,43 @@ public class EstudianteService {
 
     public void addNew(Estudiante e) {
 
-        //
+        /*Creo una función de bool para determinar cuando debo de detenerme
+        * Guardo en una varieble los datos que poseo de la base de datos de cada estudiante*/
 
         boolean condicion = false;
         var estudianteActual = this.database.getEstudianteTable();
-         //
+
+         /*Debo de recorrer cada estudiante y compararlo para que no me guarde duplicados
+         * si encuentra duplicados el ciclo se detiene debido a que encontró un carnet igual al
+         * dato ingresado.*/
+
         for (Estudiante estudianteInsert : estudianteActual){
 
-            //
             if (estudianteInsert.getCarne() == e.getCarne()){
                 condicion = true;
+                System.out.println("El estudiante ya existe");
                 break;
             }
         }
-        // jhf
+
+        /*Sí la condición es distinta lo que hace es agregar el dato que se va ingresar en la base de datos*/
         if (!condicion){
             this.database.getEstudianteTable().add(e);
         }
-        else {
-            System.out.println("El estudiante ya existe");
-        }
-
     }
 
     public void updateStudent(Estudiante e)  {
-        // Implementar codigo de actualizacion
-        // Traer el estudiante con carne X
-        // modificar el estudiante con los datos del estudiante e
 
-        //
+        /*Creo una función de bool para determinar cuando debo de detenerme
+         * Sí el carné búscado es igual a uno que exista en la base de datos este los reemplaza
+         * cabe resaltar que sólo cambia nombre, apellido y edad*/
+
         boolean condicion = false;
         var estudianteActual = this.database.getEstudianteTable();
 
-        //
+        /*El ciclo itera en toda la base de datos hasta encontrar el dato y sí no es así
+        * Envía un mensaje en pantalla */
+
         for (Estudiante estudianteUpdate : estudianteActual){
 
             if (estudianteUpdate.getCarne() == e.getCarne()){
@@ -78,15 +82,17 @@ public class EstudianteService {
     }
 
     public void deleteStudent(long carne) {
-        // implementar codigo de borrado
 
-        //
+        /*Creo una función de bool para determinar cuando debo de detenerme
+        * se procede hacer el ciclo hasta encontrar con el carné búscado y una vez
+        * encontrado se procede a borrarlo de la base de datos, sí no encuentra el carné significa
+        * que el carné no existe y este envía un mensaje en pantalla*/
+
         boolean condicion = false;
         var estudianteActual = this.database.getEstudianteTable();
 
         for (Estudiante estudianteDelete : estudianteActual){
 
-            //
             if (estudianteDelete.getCarne() == carne){
                 this.database.getEstudianteTable().remove(estudianteDelete);
                 condicion = true;
